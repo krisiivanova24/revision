f3132f07dfd5f8c8f49a8977eb51c104098096dd
krisiivanova24 <35655415+krisiivanova24@users.noreply.github.com>
2018-04-28 20:11:28 +0300

    salary

--- Program.cs
+++ Program.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BonApetit
+namespace salary
 {
     class Program
     {
         static void Main(string[] args)
         {
+            byte n = byte.Parse(Console.ReadLine());
+            List<person> people = new List<person>();
+            List<string[]> list = new List<string[]>();
+            person per = new person();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+                list.Add(input);
+
+            }
+            float percent = float.Parse(Console.ReadLine());
+            for (int i = 0; i < list.Count; i++)
+            {
+                per = new person(list[i][0], list[i][1], sbyte.Parse(list[i][2]), double.Parse(list[i][3]));
+                if (list[i][0].Length < 3 || list[i][1].Length < 3 || sbyte.Parse(list[i][2]) < 0 || double.Parse(list[i][3]) < 460.00)
+                {
+
+                }
+                else
+                {
+                    people.Add(per);
+
+                }
+                
+                
+            }
+            per.Bonus(people, percent);
+            per.Print(people);
+
         }
     }
 }
---
+++ person.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salary
+{
+    class person
+    {
+        private string firstName;
+        private string lastName;
+        private sbyte age;
+        private double salary;
+        private List<person> list;
+        private bool tryIt = false;
+        public person()
+        {
+        }
+        public person(string name, string name2, sbyte age, double salary)
+        {
+            tryIt = false;
+            if (name.Length < 3)
+            {
+                Console.WriteLine("First name cannot be less than 3 symbols");
+                
+                
+            }
+            else
+            {
+                this.firstName = name;
+            }
+            if (name2.Length < 3)
+            {
+                Console.WriteLine("Last name cannot be less than 3 symbols");
+            }
+            else
+            {
+                this.lastName = name2;
+            }
+            if (age < 0)
+            {
+                Console.WriteLine("Age cannot be zero or negative integer");
+            }
+            else
+            {
+                this.age = age;
+            }
+            if (salary < 460.00)
+            {
+                Console.WriteLine("Salary cannot be less than 460 leva");
+            }
+            else
+            {
+                this.salary = salary;
+            }
+
+
+        }
+        public List<person> List
+        {
+            get { return this.list; }
+            set { this.list = value; }
+        }
+        public void Bonus(List<person> list, double bonus)
+        {
+            foreach (var item in list)
+            {
+                if (item.age < 30)
+                {
+
+
+                    item.salary += item.salary * bonus / 2 / 100;
+
+                }
+                else
+                {
+
+                    double sum = item.salary * (bonus / 100);
+                    //Console.WriteLine(sum + " ***********************");
+                    item.salary += sum;
+
+                }
+            }
+
+        }
+        public void Print(List<person> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.firstName} {item.lastName } gets {item.salary:f2} leva");
+            }
+        }
+    }
+}
