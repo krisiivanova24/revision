e9a967995f59ea6002afb71a30a8dfe86f52672c
krisiivanova24 <35655415+krisiivanova24@users.noreply.github.com>
2018-04-28 20:16:34 +0300

    Box

---
+++ Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Box
+    {
+        private double length;
+        private double weight;
+        private double high;
+
+        public Box(double length, double weight, double high) {
+            this.length = length;
+            this.weight = weight;
+            this.high = high;                                          
+        }
+        public void Print(){
+            Console.WriteLine(3);
+            double result = 2 * this.length * weight + 2 * length * high + 2 * weight * high;
+            Console.WriteLine($"Surface Area - {result:f2}");
+            result = 2 * length * high + 2 * weight * high;
+            Console.WriteLine($"Lateral Surface Area - {result:f2}");
+            result = length * weight * high;
+            Console.WriteLine($"Volume - {result:f2}");
+        }                               
+    }
+}
+ 
--- Program.cs
+++ Program.cs
@@ -4,40 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Team
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] firstLine = Console.ReadLine().Split(';').ToArray();
-            string[] inputs = Console.ReadLine().Split(';').ToArray();
-            Team team = new Team();
-            List<Person> list = new List<Person>();
-            while (!inputs.Contains("END"))
+            //inputs
+            double length = double.Parse(Console.ReadLine());
+            double weight = double.Parse(Console.ReadLine());
+            double high = double.Parse(Console.ReadLine());
+            if (length < 0 || weight < 0 || high < 0)
             {
-                if (String.Equals(inputs[0], "Add"))
-                {
-                    Person per = new Person(inputs[2], int.Parse(inputs[3]), int.Parse(inputs[4]), int.Parse(inputs[5]), int.Parse(inputs[6]), int.Parse(inputs[7]));
-                    list.Add(per);
-                    team.Add(inputs[1], list);
-                    //team.Print2();
-                }
-                else
-                {
-                    if (String.Equals(inputs[0], "Remove") && team.List != null )
-                    {
-                        Console.WriteLine(inputs[2]);
-                        team.Remove(team,inputs[2]);
-                    }
-                    else
-                    {
-                        team.Rating();
-                    }
-                }
-                inputs = Console.ReadLine().Split(';').ToArray();
+                Console.WriteLine(3);
+                Console.WriteLine("Width cannot be zero or negative. ");
             }
-            team.Print();
+            else
+            {
+                //Boxy-box
+                Box boxy = new Box(length, weight, high);
+                boxy.Print();
+            }
+
+            
         }
     }
 }
